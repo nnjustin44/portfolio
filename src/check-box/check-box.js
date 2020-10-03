7808f1d84eac725f375dab59fce65c8fb292c653
nnjustin44 <nnjustin44@gmail.com>
2020-10-02 23:37:35 -0400

    logo flex

--- src/check-box/check-box.js
+++ src/check-box/check-box.js
@@ -3,17 +3,23 @@ import Checkbox from "@material-ui/core/Checkbox";
 import "./check-box.css";
 import logo from "../images/logo192.png";
 import javascript from "../images/javascript-logo.png";
-
+import firebase from "../images/firebase-logo.png";
 function CheckboxContainer() {
   let [checked, setChecked] = React.useState(true);
-
   let handleChange = (event) => {
     setChecked(event.target.checked);
   };
-  let [checked2, setChecked2] = React.useState(true);
 
+  let [checked2, setChecked2] = React.useState(true);
   let handleChange2 = (event) => {
     setChecked2(event.target.checked);
+    console.log(event);
+    console.log(event.target);
+  };
+
+  let [checked3, setChecked3] = React.useState(true);
+  let handleChange3 = (event) => {
+    setChecked3(event.target.checked);
     console.log(event);
     console.log(event.target);
   };
@@ -26,7 +32,7 @@ function CheckboxContainer() {
       <header className="checkbox-header">Skillset</header>{" "}
       {/*className for .css refernce*/}
       <body className="checkbox-body">
-        <p className="first-image">
+        <p className="logo">
           <img src={logo} className="react-logo"></img>
           <br></br>
           <Checkbox
@@ -38,13 +44,25 @@ function CheckboxContainer() {
           />
         </p>
 
-        <p className="second-image">
+        <p className="logo2">
           <img src={javascript} className="javascript-logo"></img>
           <br></br>
           <Checkbox
             className="check-box2"
             checked={checked2}
             onChange={handleChange2}
+            color="default"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+        </p>
+
+        <p className="logo3">
+          <img src={firebase} className="firebase-logo"></img>
+          <br></br>
+          <Checkbox
+            className="check-box3"
+            checked={checked3}
+            onChange={handleChange3}
             color="default"
             inputProps={{ "aria-label": "primary checkbox" }}
           />
